89c8fe9898ef61a74436e80989dca67ea69c00c6
pawel-bogdan <72096306+pawel-bogdan@users.noreply.github.com>
2021-05-31 12:00:25 +0200

    Delete YT_Downloader directory

--- YT_Downloader/utils.py
+++
@@ -1,8 +0,0 @@
-def resize_image(image, new_size):
-    """
-    Function resizes image to new size.
-    :param image: Image which will be resized.
-    :param new_size: Tuple which contains two values: width and height of new size
-    :return: Resized image
-    """
-    return image.resize(new_size)
